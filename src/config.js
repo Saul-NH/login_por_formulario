241e80ac1411d03b369ffcdfc03b63c11ad51129
Saul <saul.navarro.utj@gmail.com>
2022-09-11 18:09:04 -0500

    Done

--- src/config.js
+++ src/config.js
@@ -2,12 +2,12 @@ import MongoStore from 'connect-mongo';
 
 export const PORT = 8080;
 
-// export const PERSISTENCE_TYPE = 'MEMORY';
+export const PERSISTENCE_TYPE = 'MEMORY';
 // export const PERSISTENCE_TYPE = 'FILE';
-export const PERSISTENCE_TYPE = 'MONGO';
+// export const PERSISTENCE_TYPE = 'MONGO';
 
 export const MONGO_DB = {
-    URL: 'mongodb+srv://tester:1234@cluster0.owruzid.mongodb.net/?retryWrites=true&w=majority',
+    URL: 'mongodb+srv://tester:<password></password>@cluster0.owruzid.mongodb.net/?retryWrites=true&w=majority',
     config: {
         useNewUrlParser: true,
         useUnifiedTopology: true,
